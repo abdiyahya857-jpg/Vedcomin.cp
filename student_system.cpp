00f169ef25a788940a71e41900d3454b3984359f
abdiyahya857-jpg <abdiyahya857@gmail.com>
2026-04-27 21:16:42 +0300

    Update student_system.cpp

--- student_system.cpp
+++ student_system.cpp
@@ -1,29 +1,55 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
 using namespace std;
 
-struct Person {
-    char name[50];
-    int age;
-    float salary;
+struct Student {
+    int id;
+    string name;
+    string department;
+    float gpa;
 };
 
+void updateGPA(Student* s, float newGPA) {
+    if (s != nullptr) s->gpa = newGPA;
+}
+
+void computeStats(Student arr[], int size) {
+    if (size == 0) return;
+    float sum = 0, maxG = arr[0].gpa, minG = arr[0].gpa;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i].gpa;
+        if (arr[i].gpa > maxG) maxG = arr[i].gpa;
+        if (arr[i].gpa < minG) minG = arr[i].gpa;
+    }
+    cout << fixed << setprecision(2);
+    cout << "Avg: " << sum / size << "\nMax: " << maxG << "\nMin: " << minG << endl;
+}
+
+int loadRecords(Student arr[], int maxSize) {
+    ifstream f("students.txt");
+    int i = 0;
+    while (i < maxSize && f >> arr[i].id >> arr[i].name >> arr[i].department >> arr[i].gpa) i++;
+    f.close();
+    return i;
+}
+
+void saveRecords(Student arr[], int size) {
+    ofstream f("students_updated.txt");
+    for (int i = 0; i < size; i++)
+        f << arr[i].id << " " << arr[i].name << " " << arr[i].department << " " << arr[i].gpa << endl;
+    f.close();
+}
+
 int main() {
-    Person p1;
-    Person* ptr = &p1;  // pointer to struct
-
-    cout << "Enter name: ";
-    cin.get(p1.name, 50);
-
-    cout << "Enter age: ";
-    cin >> ptr->age;        // using pointer
-
-    cout << "Enter salary: ";
-    cin >> ptr->salary;     // using pointer
-
-    cout << "\n--- Information ---\n";
-    cout << "Name = " << ptr->name << endl;
-    cout << "Age = " << ptr->age << endl;
-    cout << "Salary = " << ptr->salary << endl;
-
+    Student students[100];
+    int count = loadRecords(students, 100);
+    if (count > 0) {
+        updateGPA(&students[0], 3.9);
+        computeStats(students, count);
+        saveRecords(students, count);
+    }
     return 0;
 }
